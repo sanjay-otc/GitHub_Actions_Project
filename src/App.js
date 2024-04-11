84a00a526479d1b831d4d824cb58c4c991246c72
robin singh <robin@otcengineering.com>
2024-04-11 14:21:59 +0530

    Update message

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
-          DevOps Pro
+          {process.env.REACT_APP_WELCOME_TEXT || "CI/CD for React App Deploy on GitHub Pages"} - By
+          Sanjay Aaswani
         </p>
       </header>
     </div>
